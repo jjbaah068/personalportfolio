14b92a06a6a22f392a608804818ce9afa4bf164d
James Junior Baah <jjbaah068@gmail.com>
2025-05-29 21:27:32 +0000

    chaanges to hero section

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,41 @@ document.addEventListener('scroll', function() {
     }
 });
 
+// Array with different roles to cycle through
+const roles = ["Frontend Developer", "Web Developer", "Web Designer"];
+// Select HTML element where role will be displayed
+const textElement = document.getElementById('changing-text');
+// Initialize the index to track the role
+let index = 0;
+// Define a function to change text 
+function changeText() {
+    textElement.classList.add("opacity-0");
+    setTimeout(() => {
+        textElement.textContent = roles[index];
+        textElement.classList.remove("opacity-0");
+        index = (index + 1) % roles.length;
+    }, 300);
+}
+
+// Call the function 
+changeText();
+// Set Interval 
+setInterval(changeText, 2500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Get the hamburger button element by its ID
 const toggleBtn = document.getElementById('menu-toggle');
